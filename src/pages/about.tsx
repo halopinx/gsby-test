cdea8ce7720e9806c3b23a4a7d58cc3e9da18043
Errah <errah@ypdigital.com.au>
2023-07-18 15:14:12 +0800

    add about us page

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "gatsby";
+import React from "react";
+
+
+const AboutPage: React.FC<PageProps> = () => {
+    return (
+        <h1>About Us Page</h1>
+    )
+}
+
+export default AboutPage;
